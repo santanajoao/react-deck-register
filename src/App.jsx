4193e247838851c89b5bc609332b386efe5eff3e
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2022-12-05 19:46:10 -0300

    Header implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Tryunfo</h1>
+      <div className="App">
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import styles from './Header.module.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className={ styles.Header }>
+        <img src={ logo } className={ styles.logo } alt="logo tryunfo" />
+      </header>
+    );
+  }
+}
